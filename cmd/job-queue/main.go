9981e34fc43698f1bf82c8f1551b16091eea34de
anitagarg24apr@gmail.com <anitagarg24apr@gmail.com>
2025-06-28 17:55:18 +0530

    Working Model

--- cmd/job-queue/main.go
+++ cmd/job-queue/main.go
@@ -2,24 +2,32 @@ package main
 
 import (
 	"Job-Queue/internal/config"
+	"Job-Queue/internal/model"
 	"Job-Queue/internal/routes"
 	"Job-Queue/internal/worker"
+	"Job-Queue/metrics"
 	"Job-Queue/pkg"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // The thing is, when you have a black box, you can only ever use it as a black box. But if you have a box that you understand well, you can take it into pieces, repurpose the pieces for completely different things, all without getting lost.
 
 func main() {
+	queue := model.NewRedisQueue(config.Client)
+
 	router := mux.NewRouter()
 
-	routes.RegisterJobRoutes(router)
+	routes.RegisterJobRoutes(router, queue)
+	metrics.Init()
+	router.Handle("/metrics", promhttp.Handler())
+
 	// Starting Workers
-	worker.StartWorkerPool(routes.JobService.Queue, 6)
+	worker.StartWorkerPool(routes.JobService.Queue, 10)
 
 	pkg.Log.Info(fmt.Sprintf("Listening on PORT: %v", config.Env.Port))
 
